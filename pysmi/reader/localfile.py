6a0644ab8ff8740ea814fe8ad9dfa56c0c066574
Ilya Etingof <etingof@users.noreply.github.com>
2015-04-19 12:16:13 +0000

    can search MIBs also in subdirs

--- pysmi/reader/localfile.py
+++ pysmi/reader/localfile.py
@@ -6,26 +6,38 @@ from pysmi import debug
 from pysmi import error
 
 class FileReader(AbstractReader):
-    def __init__(self, path):
+    def __init__(self, path, recursive=True):
         self._path = os.path.normpath(path)
+        self._recursive = recursive
 
     def __str__(self): return '%s{"%s"}' % (self.__class__.__name__, self._path)
 
-    def getData(self, timestamp, mibname):
-        debug.logger & debug.flagReader and debug.logger('looking for MIB %s that is newer than %s' % (mibname, time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(timestamp))))
-        for mibfile in self.getMibVariants(mibname):
-            f = os.path.join(self._path, mibfile)
-            debug.logger & debug.flagReader and debug.logger('trying MIB %s' % f)
-            if os.path.exists(f):
-                try:
-                    lastModified = os.stat(f)[8]
-                    if lastModified > timestamp:
-                        debug.logger & debug.flagReader and debug.logger('source MIB %s is new enough (%s), fetching data...' % (f, time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(lastModified))))
-                        return open(f, mode='rb').read(self.maxMibSize).decode('utf-8', 'ignore')
-                except (OSError, IOError):
-                    debug.logger & debug.flagReader and debug.logger('source file %s open failure: %s' % (f, sys.exc_info()[1]))
+    def getSubdirs(self, path, recursive):
+        if not recursive:
+            return [ path ]
+        dirs = [ path ]
+        for d in os.listdir(path):
+            d = os.path.join(path, d)
+            if os.path.isdir(d):
+                dirs.extend(self.getSubdirs(d, recursive))
+        return dirs
 
-                raise error.PySmiSourceNotModifiedError('source MIB %s is older than needed' % f, reader=self)
+    def getData(self, timestamp, mibname):
+        debug.logger & debug.flagReader and debug.logger('%slooking for MIB %s that is newer than %s' % (self._recursive and 'recursively ' or '', mibname, time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(timestamp))))
+        for path in self.getSubdirs(self._path, self._recursive):
+            for mibfile in self.getMibVariants(mibname):
+                f = os.path.join(path, mibfile)
+                debug.logger & debug.flagReader and debug.logger('trying MIB %s' % f)
+                if os.path.exists(f):
+                    try:
+                        lastModified = os.stat(f)[8]
+                        if lastModified > timestamp:
+                            debug.logger & debug.flagReader and debug.logger('source MIB %s is new enough (%s), fetching data...' % (f, time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(lastModified))))
+                            return open(f, mode='rb').read(self.maxMibSize).decode('utf-8', 'ignore')
+                    except (OSError, IOError):
+                        debug.logger & debug.flagReader and debug.logger('source file %s open failure: %s' % (f, sys.exc_info()[1]))
+
+                    raise error.PySmiSourceNotModifiedError('source MIB %s is older than needed' % f, reader=self)
 
         raise error.PySmiSourceNotFoundError('source MIB %s not found' % mibname, reader=self)
 
