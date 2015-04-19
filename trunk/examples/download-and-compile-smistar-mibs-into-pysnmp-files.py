8d6b99f1a2b21276ce9ffbe714ff23b60dd81c53
elie <elie@ad461c44-8502-4b87-8320-ac464cdaecfc>
2015-04-19 11:30:02 +0000

    example added on source MIB download from the Internet

---
+++ trunk/examples/download-and-compile-smistar-mibs-into-pysnmp-files.py
@@ -0,0 +1,51 @@
+#
+# Look up specific ASN.1 MIBs at configured Web and FTP sites,
+# compile them into pysnmp form and save Python modules as plain-text
+# files in a local directory.
+#
+# Try to support both SMIv1 and SMIv2 flavors of SMI as well as 
+# popular deviations from official syntax found in the wild.
+#
+# In this example we disable automatic dependency checking on MIB
+# compilation using noDeps flag.
+#
+# Also, we do not check if target file already exists thus MIB
+# compilation occurs on every invocation.
+#
+from pysmi.reader.httpclient import HttpReader
+from pysmi.reader.ftpclient import FtpReader
+from pysmi.searcher.stub import StubSearcher
+from pysmi.writer.pyfile import PyFileWriter
+from pysmi.parser.smiv1compat import SmiV1CompatParser
+from pysmi.codegen.pysnmp import PySnmpCodeGen, baseMibs
+from pysmi.compiler import MibCompiler
+from pysmi import debug
+
+#debug.setLogger(debug.Debug('all'))
+
+inputMibs = [ 'IF-MIB', 'IP-MIB' ]
+httpSources = [ 
+    ('www.opensource.apple.com', 80, '/source/net_snmp/net_snmp-7/net-snmp/mibs/<mib>?txt')
+]
+ftpSources = [
+    ('ftp.cisco.com', '/pub/mibs/v1/<mib>')
+]
+dstDirectory = '.pysnmp-mibs'
+
+# Initialize compiler infrastructure
+
+mibCompiler = MibCompiler(
+    SmiV1CompatParser(), PySnmpCodeGen(), PyFileWriter(dstDirectory)
+)
+
+# search for source MIBs at Web and FTP sites
+mibCompiler.addSources(*[ HttpReader(*x) for x in httpSources ])
+mibCompiler.addSources(*[ FtpReader(*x) for x in ftpSources ])
+
+# never recompile MIBs with MACROs
+mibCompiler.addSearchers(StubSearcher(*baseMibs))
+
+# run non-recursive MIB compilation
+processed = mibCompiler.compile(*inputMibs, noDeps=True)
+
+print('Created MIBs: %s' % ', '.join(processed))
