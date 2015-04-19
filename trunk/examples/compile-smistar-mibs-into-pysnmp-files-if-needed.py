eecb6776e6d60995dfa4c1c3aa8c5e0c475f82f7
elie <elie@ad461c44-8502-4b87-8320-ac464cdaecfc>
2015-04-19 11:06:15 +0000

    * user callback writer implemented * text-in text-out MIB compilation example added

--- trunk/examples/compile-smistar-mibs-into-pysnmp-files-if-needed.py
+++ trunk/examples/compile-smistar-mibs-into-pysnmp-files-if-needed.py
@@ -1,16 +1,27 @@
 #
-# Get specific ASN.1 MIBs from configured local directories,
-# compile them into pysnmp form and save Python modules as
-# plain-text files in a local directory.
+# Look up specific ASN.1 MIBs at configured local directories,
+# compile them into pysnmp form if not done yet and save Python
+# modules as plain-text files in a local directory.
 #
-# Assume source MIBs are SMIv2-compliant and syntaxically valid.
+# Try to support both SMIv1 and SMIv2 flavors of SMI as well as 
+# popular deviations from official syntax found in the wild.
+#
+# When figuring out if compilation is needed, check all known
+# places where pysnmp MIBs could possibly be found.
+#
+# You can force MIB re-compilation by passing rebuild flag to
+# MIB compiler (see below).
+#
+# Default invocation of MIB compiler does not generate [potentially
+# large] comments and texts found in MIBs. If you need them in pysnmp
+# MIB modules, just pass genTexts flag to MIB compiler.
 # 
 from pysmi.reader.localfile import FileReader
 from pysmi.searcher.pyfile import PyFileSearcher
 from pysmi.searcher.pypackage import PyPackageSearcher
 from pysmi.searcher.stub import StubSearcher
 from pysmi.writer.pyfile import PyFileWriter
-from pysmi.parser.smiv2 import SmiV2Parser
+from pysmi.parser.smiv1compat import SmiV1CompatParser
 from pysmi.codegen.pysnmp import PySnmpCodeGen, defaultMibPackages, baseMibs
 from pysmi.compiler import MibCompiler
 from pysmi import debug
@@ -23,7 +34,7 @@ dstDirectory = '.pysnmp-mibs'
 
 # Initialize compiler infrastructure
 
-mibCompiler = MibCompiler(SmiV2Parser(),
+mibCompiler = MibCompiler(SmiV1CompatParser(),
                           PySnmpCodeGen(),
                           PyFileWriter(dstDirectory))
 
@@ -39,6 +50,6 @@ mibCompiler.addSearchers(*[ PyPackageSearcher(x) for x in defaultMibPackages ])
 mibCompiler.addSearchers(StubSearcher(*baseMibs))
 
 # run [possibly recursive] MIB compilation
-processed = mibCompiler.compile(*inputMibs)
+processed = mibCompiler.compile(*inputMibs)  #, rebuild=True, genTexts=True)
 
 print('Created/updated MIBs: %s' % ', '.join(processed))
---
+++ trunk/examples/compile-smiv2-mibs-from-text-into-pysnmp-code.py
@@ -0,0 +1,43 @@
+#
+# Invoke user callback function to provide MIB text,
+# compile given text string into pysnmp MIB form and pass
+# results to another user callback function for storing.
+#
+# Here we expect to deal only with SMIv2-valid MIBs.
+#
+# We use noDeps flag to prevent MIB compiler from attemping
+# to compile IMPORT'ed MIBs as well.
+#
+import sys
+from pysmi.reader.callback import CallbackReader
+from pysmi.searcher.stub import StubSearcher
+from pysmi.writer.callback import CallbackWriter
+from pysmi.parser.smiv2 import SmiV2Parser
+from pysmi.codegen.pysnmp import PySnmpCodeGen, baseMibs
+from pysmi.compiler import MibCompiler
+from pysmi import debug
+
+#debug.setLogger(debug.Debug('all'))
+
+inputMibs = [ 'IF-MIB', 'IP-MIB' ]
+srcDir = '/usr/share/snmp/mibs/'  # we will read MIBs from here
+
+# Initialize compiler infrastructure
+
+mibCompiler = MibCompiler(
+    SmiV2Parser(),
+    PySnmpCodeGen(), 
+    # out own callback function stores results in its own way
+    CallbackWriter(lambda m,d,a,c: sys.stdout.write(d))
+)
+
+# our own callback function serves as a MIB source here
+mibCompiler.addSources(
+  CallbackReader(lambda t,m,c: open(srcDir+m+'.txt').read())
+)
+
+# never recompile MIBs with MACROs
+mibCompiler.addSearchers(StubSearcher(*baseMibs))
+
+# run non-recursive MIB compilation
+mibCompiler.compile(*inputMibs, noDeps=True)
