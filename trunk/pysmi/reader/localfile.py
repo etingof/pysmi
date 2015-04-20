84d6dddd290c392f0590b03d8194b09a7810ff62
elie <elie@ad461c44-8502-4b87-8320-ac464cdaecfc>
2015-04-20 07:57:53 +0000

    conditionally ignore access errors

--- trunk/pysmi/reader/localfile.py
+++ trunk/pysmi/reader/localfile.py
@@ -6,17 +6,25 @@ from pysmi import debug
 from pysmi import error
 
 class FileReader(AbstractReader):
-    def __init__(self, path, recursive=True):
+    def __init__(self, path, recursive=True, ignoreErrors=True):
         self._path = os.path.normpath(path)
         self._recursive = recursive
+        self._ignoreErrors = ignoreErrors
 
     def __str__(self): return '%s{"%s"}' % (self.__class__.__name__, self._path)
 
-    def getSubdirs(self, path, recursive):
+    def getSubdirs(self, path, recursive=True, ignoreErrors=True):
         if not recursive:
             return [ path ]
         dirs = [ path ]
-        for d in os.listdir(path):
+        try:
+            subdirs = os.listdir(path)
+        except OSError:
+            if ignoreErrors:
+                return dirs
+            else:
+                raise error.PySmiError('directory %s access error: %s' % (path, sys.exc_info()[1]))
+        for d in subdirs:
             d = os.path.join(path, d)
             if os.path.isdir(d):
                 dirs.extend(self.getSubdirs(d, recursive))
@@ -24,7 +32,8 @@ class FileReader(AbstractReader):
 
     def getData(self, timestamp, mibname):
         debug.logger & debug.flagReader and debug.logger('%slooking for MIB %s that is newer than %s' % (self._recursive and 'recursively ' or '', mibname, time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(timestamp))))
-        for path in self.getSubdirs(self._path, self._recursive):
+        for path in self.getSubdirs(self._path, self._recursive,
+                                    self._ignoreErrors):
             for mibfile in self.getMibVariants(mibname):
                 f = os.path.join(path, mibfile)
                 debug.logger & debug.flagReader and debug.logger('trying MIB %s' % f)
@@ -36,6 +45,8 @@ class FileReader(AbstractReader):
                             return open(f, mode='rb').read(self.maxMibSize).decode('utf-8', 'ignore')
                     except (OSError, IOError):
                         debug.logger & debug.flagReader and debug.logger('source file %s open failure: %s' % (f, sys.exc_info()[1]))
+                        if not self._ignoreErrors:
+                            raise error.PySmiError('file %s access error: %s' % (f, sys.exc_info()[1]))
 
                     raise error.PySmiSourceNotModifiedError('source MIB %s is older than needed' % f, reader=self)
 
