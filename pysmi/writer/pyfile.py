1f48491e05da4bf4346d5ecf3157b63e5ec3261c
Ilya Etingof <etingof@users.noreply.github.com>
2015-04-21 18:58:35 +0000

    attempt to hardlink alias files, fall back to full copy on linking failure

--- pysmi/writer/pyfile.py
+++ pysmi/writer/pyfile.py
@@ -29,23 +29,36 @@ class PyFileWriter(AbstractWriter):
             except OSError:
                 raise error.PySmiWriterError('failure creating destination directory %s: %s' % (self._path, sys.exc_info()[1]), writer=self)
 
+        pyfile = os.path.join(self._path, mibname) + self.suffixes[imp.PY_SOURCE][0][0]
+        try:
+            f = open(pyfile, 'wb')
+            f.write(data.encode('utf-8'))
+            f.close()
+        except (IOError, UnicodeEncodeError):
+            raise error.PySmiWriterError('failure writing file %s: %s' % (pyfile, sys.exc_info()[1]), file=pyfile, writer=self)
+
+        debug.logger & debug.flagWriter and debug.logger('created file %s' % pyfile)
+
+        if alias:
+            pyalias = os.path.join(self._path, alias) + self.suffixes[imp.PY_SOURCE][0][0]
+            try:
+                os.unlink(pyalias)
+                os.link(pyfile, pyalias)
+            except (AttributeError, OSError):  # not all platforms support it
+                try:
+                    f = open(pyalias, 'wb')
+                    f.write(data.encode('utf-8'))
+                    f.close()
+                except (IOError, UnicodeEncodeError):
+                    raise error.PySmiWriterError('failure writing file %s: %s' % (pyalias, sys.exc_info()[1]), file=pyalias, alias=true, writer=self)
+                else:
+                    debug.logger & debug.flagWriter and debug.logger('a copy of file %s created as %s' % (pyfile, pyalias))
+            else:
+                debug.logger & debug.flagWriter and debug.logger('file %s linked to %s' % (pyalias, pyfile))
+        
         for filename in mibname, alias:
             if not filename:
                 continue
-            pyfile = os.path.join(self._path, filename) + self.suffixes[imp.PY_SOURCE][0][0]
-            try:
-                f = open(pyfile, 'wb')
-                f.write(data.encode('utf-8'))
-                f.close()
-            except (IOError, UnicodeEncodeError):
-                try:
-                    os.unlink(pyfile)
-                except:
-                    pass
-                raise error.PySmiWriterError('failure writing file %s: %s' % (pyfile, sys.exc_info()[1]), file=pyfile, writer=self)
-
-            debug.logger & debug.flagWriter and debug.logger('file %s created' % pyfile)
-
             try:
                 py_compile.compile(pyfile, doraise=True)
             except (SyntaxError, py_compile.PyCompileError):
@@ -55,9 +68,9 @@ class PyFileWriter(AbstractWriter):
                     os.unlink(pyfile)
                 except:
                     pass
-                raise error.PySmiWriterError('failure compiling %s: %s' % (pyfile, sys.exc_info()[1]), file=pyfile, writer=self)
+                raise error.PySmiWriterError('failure compiling %s: %s' % (filename, sys.exc_info()[1]), file=filename, writer=self)
 
-            debug.logger & debug.flagWriter and debug.logger('compiled %s' % pyfile)
+            debug.logger & debug.flagWriter and debug.logger('compiled %s' % filename)
 
 if __name__ == '__main__':
     from pysmi import debug
