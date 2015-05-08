7a9e3e60e60f371d857d6d0ac50706b1937b68b0
elie <elie@ad461c44-8502-4b87-8320-ac464cdaecfc>
2015-05-08 20:25:01 +0000

    moved grammar relaxation methods to individual scopes to avoid clashes

--- trunk/pysmi/parser/smi.py
+++ trunk/pysmi/parser/smi.py
@@ -1127,204 +1127,216 @@ class SmiV2Parser(AbstractParser):
 # SMIv1 grammar
 #
 
-# NETWORKADDRESS added
-def p_importedKeyword(self, p): 
-  """importedKeyword : importedSMIKeyword
-                     | BITS
-                     | INTEGER32
-                     | IPADDRESS
-                     | NETWORKADDRESS
-                     | MANDATORY_GROUPS
-                     | MODULE_COMPLIANCE
-                     | MODULE_IDENTITY
-                     | OBJECT_GROUP
-                     | OBJECT_IDENTITY
-                     | OBJECT_TYPE
-                     | OPAQUE
-                     | TEXTUAL_CONVENTION
-                     | TIMETICKS
-                     | UNSIGNED32"""
-  p[0] = p[1]
-
-# NETWORKADDRESS added
-def p_typeSMIandSPPI(self, p):
-  """typeSMIandSPPI : IPADDRESS
-                    | NETWORKADDRESS
-                    | TIMETICKS
-                    | OPAQUE
-                    | INTEGER32
-                    | UNSIGNED32"""
-  p[0] = p[1]
-
-# NETWORKADDRESS added
-def p_ApplicationSyntax(self, p):
-  """ApplicationSyntax : IPADDRESS anySubType
-                       | NETWORKADDRESS anySubType
-                       | COUNTER32
-                       | COUNTER32 integerSubType
-                       | GAUGE32
-                       | GAUGE32 integerSubType
-                       | UNSIGNED32
-                       | UNSIGNED32 integerSubType
-                       | TIMETICKS anySubType
-                       | OPAQUE
-                       | OPAQUE octetStringSubType
-                       | COUNTER64
-                       | COUNTER64 integerSubType"""
-  n = len(p)
-  if n == 2:                                                                  
-    p[0] = ('ApplicationSyntax', p[1])                                        
-  elif n == 3:
-    p[0] = ('ApplicationSyntax', p[1], p[2])
-
-# SMIv1 IndexTypes added
-def p_Index(self, p):
-  """Index : ObjectName
-           | typeSMIv1"""
-             
-  # libsmi: TODO: use the SYNTAX value of the correspondent 
-  #               OBJECT-TYPE invocation
-  p[0] = isinstance(p[1], tuple) and p[1][1][0] or p[1]
-
-# for Index rule
-def p_typeSMIv1(self, p):
-  """typeSMIv1 : INTEGER
-               | OCTET STRING
-               | IPADDRESS
-               | NETWORKADDRESS"""
-  n = len(p)
-  indextype = n == 3 and p[1] + ' ' + p[2] or p[1]
-  p[0] = indextype
-
-# NETWORKADDRESS added for SEQUENCE syntax
-def p_sequenceApplicationSyntax(self, p):
-  """sequenceApplicationSyntax : IPADDRESS anySubType
-                               | NETWORKADDRESS anySubType
-                               | COUNTER32 anySubType
-                               | GAUGE32 anySubType
-                               | UNSIGNED32 anySubType
-                               | TIMETICKS anySubType
-                               | OPAQUE
-                               | COUNTER64 anySubType"""
-  n = len(p)
-  if n == 2:
+class SupportNetworkAddress:
+  # NETWORKADDRESS added
+  def p_importedKeyword(self, p): 
+    """importedKeyword : importedSMIKeyword
+		       | BITS
+		       | INTEGER32
+		       | IPADDRESS
+		       | NETWORKADDRESS
+		       | MANDATORY_GROUPS
+		       | MODULE_COMPLIANCE
+		       | MODULE_IDENTITY
+		       | OBJECT_GROUP
+		       | OBJECT_IDENTITY
+		       | OBJECT_TYPE
+		       | OPAQUE
+		       | TEXTUAL_CONVENTION
+		       | TIMETICKS
+		       | UNSIGNED32"""
     p[0] = p[1]
-  elif n == 3:
-    p[0] = p[1] # XXX not supporting subtypes here
+
+  # NETWORKADDRESS added
+  def p_typeSMIandSPPI(self, p):
+    """typeSMIandSPPI : IPADDRESS
+		      | NETWORKADDRESS
+		      | TIMETICKS
+		      | OPAQUE
+		      | INTEGER32
+		      | UNSIGNED32"""
+    p[0] = p[1]
+
+  # NETWORKADDRESS added
+  def p_ApplicationSyntax(self, p):
+    """ApplicationSyntax : IPADDRESS anySubType
+			 | NETWORKADDRESS anySubType
+			 | COUNTER32
+			 | COUNTER32 integerSubType
+			 | GAUGE32
+			 | GAUGE32 integerSubType
+			 | UNSIGNED32
+			 | UNSIGNED32 integerSubType
+			 | TIMETICKS anySubType
+			 | OPAQUE
+			 | OPAQUE octetStringSubType
+			 | COUNTER64
+			 | COUNTER64 integerSubType"""
+    n = len(p)
+    if n == 2:                                                                  
+      p[0] = ('ApplicationSyntax', p[1])                                        
+    elif n == 3:
+      p[0] = ('ApplicationSyntax', p[1], p[2])
+
+  # NETWORKADDRESS added for SEQUENCE syntax
+  def p_sequenceApplicationSyntax(self, p):
+    """sequenceApplicationSyntax : IPADDRESS anySubType
+				 | NETWORKADDRESS anySubType
+				 | COUNTER32 anySubType
+				 | GAUGE32 anySubType
+				 | UNSIGNED32 anySubType
+				 | TIMETICKS anySubType
+				 | OPAQUE
+				 | COUNTER64 anySubType"""
+    n = len(p)
+    if n == 2:
+      p[0] = p[1]
+    elif n == 3:
+      p[0] = p[1] # XXX not supporting subtypes here
+
+class SupportIndex:
+  # SMIv1 IndexTypes added
+  def p_Index(self, p):
+    """Index : ObjectName
+	     | typeSMIv1"""
+	       
+    # libsmi: TODO: use the SYNTAX value of the correspondent 
+    #               OBJECT-TYPE invocation
+    p[0] = isinstance(p[1], tuple) and p[1][1][0] or p[1]
+
+  # for Index rule
+  def p_typeSMIv1(self, p):
+    """typeSMIv1 : INTEGER
+		 | OCTET STRING
+		 | IPADDRESS
+		 | NETWORKADDRESS"""
+    n = len(p)
+    indextype = n == 3 and p[1] + ' ' + p[2] or p[1]
+    p[0] = indextype
 
 #
 # Some changes in grammar to handle common mistakes in MIBs 
 #
 
-# comma at the end of import list
-def p_importIdentifiers(self, p):
-  """importIdentifiers : importIdentifiers ',' importIdentifier
-                       | importIdentifier
-                       | importIdentifiers ','"""
-  n = len(p)
-  if n == 4:
-    p[0] = p[1] + [p[3]]
-  elif n == 2:
-    p[0] = [p[1]]
-  elif n == 3: # excessive comma case 
-    p[0] = p[1]
-
-# comma at the end of sequence list
-def p_sequenceItems(self, p):
-  """sequenceItems : sequenceItems ',' sequenceItem
-                   | sequenceItem
-                   | sequenceItems ','""" 
-  # libsmi: TODO: might this list be emtpy?
-  n = len(p)
-  if n == 4:
-    p[0] = p[1] + [p[3]]
-  elif n == 2:
-    p[0] = [p[1]]
-  elif n == 3: # excessive comma case 
-    p[0] = p[1]
-
-# common typos handled (mix of commas and spaces)
-def p_enumItems(self, p):
-  """enumItems : enumItems ',' enumItem
-               | enumItem
-               | enumItems enumItem
-               | enumItems ','"""
-  n = len(p)
-  if n == 4:
-    p[0] = p[1] + [p[3]]
-  elif n == 2:
-    p[0] = [p[1]]
-  elif n == 3: # typo case
-    if p[2] == ',':
+class CommaInImport:
+  # comma at the end of import list
+  def p_importIdentifiers(self, p):
+    """importIdentifiers : importIdentifiers ',' importIdentifier
+			 | importIdentifier
+			 | importIdentifiers ','"""
+    n = len(p)
+    if n == 4:
+      p[0] = p[1] + [p[3]]
+    elif n == 2:
+      p[0] = [p[1]]
+    elif n == 3: # excessive comma case 
       p[0] = p[1]
-    else:
-      p[0] = p[1] + [p[2]]
 
-# common mistake - using UPPERCASE_IDENTIFIER
-def p_enumItem(self, p):
-  """enumItem : LOWERCASE_IDENTIFIER '(' enumNumber ')'
-              | UPPERCASE_IDENTIFIER '(' enumNumber ')'"""
-  p[0] = (p[1], p[3])
+class CommaInSequence:
+  # comma at the end of sequence list
+  def p_sequenceItems(self, p):
+    """sequenceItems : sequenceItems ',' sequenceItem
+		     | sequenceItem
+		     | sequenceItems ','""" 
+    # libsmi: TODO: might this list be emtpy?
+    n = len(p)
+    if n == 4:
+      p[0] = p[1] + [p[3]]
+    elif n == 2:
+      p[0] = [p[1]]
+    elif n == 3: # excessive comma case 
+      p[0] = p[1]
 
-# common mistake - LOWERCASE_IDENTIFIER in symbol's name
-def p_notificationTypeClause(self, p):
-  """notificationTypeClause : fuzzy_lowercase_identifier NOTIFICATION_TYPE NotificationObjectsPart STATUS Status DESCRIPTION Text ReferPart COLON_COLON_EQUAL '{' NotificationName '}'""" # some MIBs have uppercase and/or lowercase id
-  p[0] = ('notificationTypeClause', p[1], # id
-                                  #  p[2], # NOTIFICATION_TYPE
-                                    p[3], # NotificationObjectsPart
-                                  #  p[5], # status
-                                    (p[6], p[7]), # description
-                                  #  p[8], # ReferPart
-                                    p[11]) # NoficationName aka objectIdentifier
+class CommaAndSpaces:
+  # common typos handled (mix of commas and spaces)
+  def p_enumItems(self, p):
+    """enumItems : enumItems ',' enumItem
+		 | enumItem
+		 | enumItems enumItem
+		 | enumItems ','"""
+    n = len(p)
+    if n == 4:
+      p[0] = p[1] + [p[3]]
+    elif n == 2:
+      p[0] = [p[1]]
+    elif n == 3: # typo case
+      if p[2] == ',':
+	p[0] = p[1]
+      else:
+	p[0] = p[1] + [p[2]]
 
-# common mistake - curly brackets around enterprise symbol
-def p_trapTypeClause(self, p):
-  """trapTypeClause : fuzzy_lowercase_identifier TRAP_TYPE EnterprisePart VarPart DescrPart ReferPart COLON_COLON_EQUAL NUMBER"""
-  # libsmi: TODO: range of number?
-  p[0] = ('trapTypeClause', p[1], # fuzzy_lowercase_identifier
-                          #  p[2], # TRAP_TYPE
-                            p[3], # EnterprisePart (objectIdentifier)
-                            p[4], # VarPart
-                            p[5], # description
-                          #  p[6], # reference
-                            p[8]) # NUMBER
-
-def p_EnterprisePart(self, p):
-  """EnterprisePart : ENTERPRISE objectIdentifier
-                    | ENTERPRISE '{' objectIdentifier '}'"""
-  n = len(p)
-  if n == 3:
-    p[0] = p[2]
-  elif n == 5: # common mistake case
-    p[0] = p[3]
-
-# common mistake - no Cells
-def p_CreationPart(self, p):
-  """CreationPart : CREATION_REQUIRES '{' Cells '}'
-                  | CREATION_REQUIRES '{' '}'
-                  | empty"""
-  n = len(p)
-  if n == 5:
+class UppercaseIdentifier:
+  # common mistake - using UPPERCASE_IDENTIFIER
+  def p_enumItem(self, p):
+    """enumItem : LOWERCASE_IDENTIFIER '(' enumNumber ')'
+		| UPPERCASE_IDENTIFIER '(' enumNumber ')'"""
     p[0] = (p[1], p[3])
+
+class LowcaseIdentifier:
+  # common mistake - LOWERCASE_IDENTIFIER in symbol's name
+  def p_notificationTypeClause(self, p):
+    """notificationTypeClause : fuzzy_lowercase_identifier NOTIFICATION_TYPE NotificationObjectsPart STATUS Status DESCRIPTION Text ReferPart COLON_COLON_EQUAL '{' NotificationName '}'""" # some MIBs have uppercase and/or lowercase id
+    p[0] = ('notificationTypeClause', p[1], # id
+				    #  p[2], # NOTIFICATION_TYPE
+				      p[3], # NotificationObjectsPart
+				    #  p[5], # status
+				      (p[6], p[7]), # description
+				    #  p[8], # ReferPart
+				      p[11]) # NoficationName aka objectIdentifier
+
+class CurlyBracesInEnterprises:
+  # common mistake - curly brackets around enterprise symbol
+  def p_trapTypeClause(self, p):
+    """trapTypeClause : fuzzy_lowercase_identifier TRAP_TYPE EnterprisePart VarPart DescrPart ReferPart COLON_COLON_EQUAL NUMBER"""
+    # libsmi: TODO: range of number?
+    p[0] = ('trapTypeClause', p[1], # fuzzy_lowercase_identifier
+			    #  p[2], # TRAP_TYPE
+			      p[3], # EnterprisePart (objectIdentifier)
+			      p[4], # VarPart
+			      p[5], # description
+			    #  p[6], # reference
+			      p[8]) # NUMBER
+
+  def p_EnterprisePart(self, p):
+    """EnterprisePart : ENTERPRISE objectIdentifier
+		      | ENTERPRISE '{' objectIdentifier '}'"""
+    n = len(p)
+    if n == 3:
+      p[0] = p[2]
+    elif n == 5: # common mistake case
+      p[0] = p[3]
+
+class NoCells:
+  # common mistake - no Cells
+  def p_CreationPart(self, p):
+    """CreationPart : CREATION_REQUIRES '{' Cells '}'
+		    | CREATION_REQUIRES '{' '}'
+		    | empty"""
+    n = len(p)
+    if n == 5:
+      p[0] = (p[1], p[3])
 
 relaxedGrammar = {
     'supportNetworkAddress': [
-        p_importedKeyword,
-        p_typeSMIandSPPI,
-        p_sequenceApplicationSyntax,
-        p_ApplicationSyntax,
+        SupportNetworkAddress.p_importedKeyword,
+        SupportNetworkAddress.p_typeSMIandSPPI,
+        SupportNetworkAddress.p_ApplicationSyntax,
+        SupportNetworkAddress.p_sequenceApplicationSyntax
     ],
     'supportIndex': [
-        p_Index,
-        p_typeSMIv1
+        SupportIndex.p_Index,
+        SupportIndex.p_typeSMIv1
     ],
-    'commaAtTheEndOfImport': [ p_importIdentifiers ],
-    'commaAtTheEndOfSequence': [ p_sequenceItems ],
-    'mixOfCommasAndSpaces': [ p_enumItems ],
-    'uppercaseIdentifier': [ p_enumItem ],
-    'lowcaseIdentifier': [ p_notificationTypeClause ],
-    'curlyBracesAroundEnterpriseInTrap': [ p_trapTypeClause, p_EnterprisePart ],
-    'noCells': [ p_CreationPart ]
+    'commaAtTheEndOfImport': [ CommaInImport.p_importIdentifiers ],
+    'commaAtTheEndOfSequence': [ CommaInSequence.p_sequenceItems ],
+    'mixOfCommasAndSpaces': [ CommaAndSpaces.p_enumItems ],
+    'uppercaseIdentifier': [ UppercaseIdentifier.p_enumItem ],
+    'lowcaseIdentifier': [ LowcaseIdentifier.p_notificationTypeClause ],
+    'curlyBracesAroundEnterpriseInTrap': [ 
+        CurlyBracesInEnterprises.p_trapTypeClause,
+        CurlyBracesInEnterprises.p_EnterprisePart 
+    ],
+    'noCells': [ NoCells.p_CreationPart ]
 }
 
 requiredLexers = {
