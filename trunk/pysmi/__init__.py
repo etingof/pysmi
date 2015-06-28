44fdeb7d3df7328e6cd115b1cb90dadc13be042a
elie <elie@ad461c44-8502-4b87-8320-ac464cdaecfc>
2015-06-28 21:27:00 +0000

    version bump due to lost Sphinx config

--- trunk/pysmi/__init__.py
+++ trunk/pysmi/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
 # http://www.python.org/dev/peps/pep-0396/
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 if sys.version_info[:2] < (2, 4):
     raise RuntimeError('PySMI requires Python 2.4 or later')
