777f735fb1c3a64ca9970dc03497ec1e6f3e0179
Ilya Etingof <etingof@gmail.com>
2017-03-30 01:28:47 +0200

    missing tests added

--- tests/__main__.py
+++ tests/__main__.py
@@ -11,18 +11,20 @@ except ImportError:
     import unittest
 
 suite = unittest.TestLoader().loadTestsFromNames(
-    ['test_moduleidentity_smiv2_pysnmp',
-    'test_imports_smiv2_pysnmp',
-    'test_agentcapabilities_smiv2_pysnmp',
-    'test_modulecompliance_smiv2_pysnmp',
-    'test_notificationgroup_smiv2_pysnmp',
-    'test_notificationtype_smiv2_pysnmp',
-    'test_objectgroup_smiv2_pysnmp',
-    'test_objecttype_smiv2_pysnmp',
-    'test_traptype_smiv2_pysnmp',
-    'test_typedeclaration_smiv2_pysnmp',
-    'test_typedeclaration_smiv1_pysnmp',
-    'test_valuedeclaration_smiv2_pysnmp']
+    ['test_agentcapabilities_smiv2_pysnmp',
+     'test_imports_smiv2_pysnmp',
+     'test_modulecompliance_smiv2_pysnmp',
+     'test_moduleidentity_smiv2_pysnmp',
+     'test_notificationgroup_smiv2_pysnmp',
+     'test_notificationtype_smiv2_pysnmp',
+     'test_objectgroup_smiv2_pysnmp',
+     'test_objectidentity_smiv2_pysnmp',
+     'test_objecttype_smiv2_pysnmp',
+     'test_smiv1_smiv2_pysnmp',
+     'test_traptype_smiv2_pysnmp',
+     'test_typedeclaration_smiv1_pysnmp',
+     'test_typedeclaration_smiv2_pysnmp',
+     'test_valuedeclaration_smiv2_pysnmp']
 )
 
 
