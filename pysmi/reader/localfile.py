4214f202103c88afd6638682f08317eb3251a899
Ilya Etingof <etingof@gmail.com>
2020-03-21 19:53:14 +0100

    Extend copyright notice to year 2020

--- pysmi/reader/localfile.py
+++ pysmi/reader/localfile.py
@@ -1,7 +1,7 @@
 #
 # This file is part of pysmi software.
 #
-# Copyright (c) 2015-2019, Ilya Etingof <etingof@gmail.com>
+# Copyright (c) 2015-2020, Ilya Etingof <etingof@gmail.com>
 # License: http://snmplabs.com/pysmi/license.html
 #
 import os
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 # This file is part of pysmi software.
 #
-# Copyright (c) 2015-2019, Ilya Etingof <etingof@gmail.com>
+# Copyright (c) 2015-2020, Ilya Etingof <etingof@gmail.com>
 # License: http://snmplabs.com/pysmi/license.html
 #
 """SNMP SMI/MIB Parser
